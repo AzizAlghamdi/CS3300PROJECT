57ec9d4d24f71485c42f765ed60026670300664d
Aziz Alghamdi <123228478+AzizAlghamdi@users.noreply.github.com>
2024-07-06 01:14:09 -0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(150), nullable=False)
+
+class LabResult(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    result_data = db.Column(db.Text, nullable=False)
+    encrypted_result = db.Column(db.Text, nullable=False)
